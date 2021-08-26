e51283232e00caade28003469941d60fa07be11f
jalgas <64311368+jalgas-kadyr@users.noreply.github.com>
2021-08-26 13:59:55 +0600

    Update ttest.py

--- ttest.py
+++ ttest.py
@@ -19,7 +19,25 @@ def current_rate():
     rates = {}
     data = json.loads(requests.get('https://back.halykbank.kz/common/currency-history').text)
     res = next(iter(data['data']['currencyHistory']))
-    rates = data['data']['currencyHistory'][res]['cards']['USD/KZT']
+    try:
+        rates = data['data']['currencyHistory']['3']['cards']['USD/KZT']
+    except Exception as error:
+        try:
+            rates = data['data']['currencyHistory'][1]['cards']['USD/KZT']
+        except Exception as error:
+            try:
+                rates = data['data']['currencyHistory']['0']['cards']['USD/KZT']
+            except Exception as error:
+                try:
+                    rates = data['data']['currencyHistory'][0]['cards']['USD/KZT']
+                except Exception as error:
+                    try:
+                        rates = data['data']['currencyHistory']['2']['cards']['USD/KZT']
+                    except Exception as error:
+                        try:
+                            rates = data['data']['currencyHistory'][2]['cards']['USD/KZT']
+                        except Exception as error:
+                            pass
 
 
 def interval():
